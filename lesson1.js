882aa41953a284abc92f08e22da5a2cd3d52c48b
Hiromi Yamamoto <hiromi.ymmt.0608@gmail.com>
2025-11-10 12:09:12 +1300

    11月10日課題

---
+++ lesson1.js
@@ -0,0 +1,16 @@
+// 配列を作る
+let fruits = ['apple', 'banana', 'orange'];
+
+// pushで追加
+fruits.push('grape');
+
+// popで最後の要素削除
+fruits.pop();
+
+// shiftで先頭削除
+fruits.shift();
+
+// unshiftで先頭に追加
+fruits.unshift('melon');
+
+console.log(fruits); // 出力: ['melon', 'banana', 'orange']
